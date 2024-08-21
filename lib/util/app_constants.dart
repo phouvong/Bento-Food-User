6045074dcb26d2b6ba1f6154481920dcd8349dba
phouvong <pphouvong@gmail.com>
2024-08-21 13:07:09 +0700

    update

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -8,7 +8,7 @@ class AppConstants {
 
   static const String fontFamily = 'Lao';
   static const bool payInWevView = false;
-  static const String webHostedUrl = 'https://food.naplao.com';
+  static const String webHostedUrl = 'https://food.bentodelivery.net';
   static const bool useReactWebsite = true;
 
   static const String baseUrl = 'https://food.naplao.com';
