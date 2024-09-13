86cd7ea0c08a2a505579e5f94ea26443f52f5c45
phouvong <pphouvong@gmail.com>
2024-09-14 00:26:46 +0700

    delete

--- lib/features/home/domain/repositories/advertisement_repository_interface.dart
+++
@@ -1,5 +0,0 @@
-import 'package:stackfood_multivendor/interface/repository_interface.dart';
-
-abstract class AdvertisementRepositoryInterface extends RepositoryInterface{
-
-}
--- lib/features/home/domain/services/advertisement_service_interface.dart
+++
@@ -1,5 +0,0 @@
-import 'package:stackfood_multivendor/features/home/domain/models/advertisement_model.dart';
-
-abstract class AdvertisementServiceInterface {
-  Future<List<AdvertisementModel>?> getAdvertisementList();
-}
